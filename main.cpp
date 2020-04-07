b7b3fb321bd5670c16a425c8afdb1e50942e326b
Jacob-Mowat <zaxquit@protonmail.com>
2020-04-07 06:13:30 +0100

    Added main.cpp, added inital CMakeLists file.

---
+++ main.cpp
@@ -0,0 +1,12 @@
+//
+// Created by Jacob on 07/04/2020.
+//
+
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char **argv) {
+    cout << "Starting virtuCPU" << endl;
+    return 0;
+}
